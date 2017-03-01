d461bad7a0dee3205dbfcd51dd40dddb248af83d
GEDDE5 <geddes.3754@gmail.com>
2017-03-01 14:48:01 -0800

    Fixes indentation

--- db/migrations/20170301220658_create_votes_table.js
+++ db/migrations/20170301220658_create_votes_table.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex, Promise) {
- return knex.schema.createTable('votes', function (table) {
-   table.increments();
-   table.integer('rank');
-   table.foreign('voter_id').references('voters.id');
-   table.foreign('choice_id').references('choice.id');
- });
+  return knex.schema.createTable('votes', function (table) {
+    table.increments();
+    table.integer('rank');
+    table.foreign('voter_id').references('voters.id');
+    table.foreign('choice_id').references('choice.id');
+  });
 };
 
 exports.down = function(knex, Promise) {
- return knex.schema.dropTable('votes');
+  return knex.schema.dropTable('votes');
 };
