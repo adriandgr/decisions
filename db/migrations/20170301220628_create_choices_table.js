0180aa5be9c1b2d141cb37066bf99eb9a6688410
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-01 22:27:11 +0000

    create_choices_table migration

--- db/migrations/20170301220628_create_choices_table.js
+++ db/migrations/20170301220628_create_choices_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function (table) {
+    table.increments();
+    table.string('name');
+    table.enu('type', ['plaintext', 'uri/image']);
+    table.string('poll_id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
