3165f646eadf6a947f6d73e469f1cebb6407f46b
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-01 22:21:44 +0000

    create_voters_table migration

--- db/migrations/20170301220638_create_voters_table.js
+++ db/migrations/20170301220638_create_voters_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('voters', function (table) {
+    table.increments();
+    table.string('name');
+    table.integer('poll_id');
+    table.string('voter_uuid');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('voters');
 };
