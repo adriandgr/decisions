88f585d47437059e2adf91f3c707a70154ff3a89
GEDDE5 <geddes.3754@gmail.com>
2017-03-01 14:28:44 -0800

    Adds migration create_polls_table

--- db/migrations/20170301220614_create_polls_table.js
+++ db/migrations/20170301220614_create_polls_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('polls', table => {
+    table.increments();
+    table.string( 'name');
+    table.string('created_by');
+    table.string('creator_email');
+    table.string('admin_uuid');
+    table.string('public_uuid');
+    table.timestamps();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('polls');
 };
