e788f998978dbfae80719aefd2e853ba810fd5e8
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-02 06:59:55 +0000

    base css

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
+const PORT        = process.env.PORT || 5000;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
+const path        = require('path');
 const app         = express();
 
 const knexConfig  = require("./knexfile");
@@ -27,11 +28,12 @@ app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
+app.use(sass({
+  src: path.join(__dirname, '/sass'),
+  dest: path.join(__dirname, '/public/styles'),
   debug: true,
-  outputStyle: 'expanded'
+  outputStyle: 'expanded',
+  prefix: '/styles'
 }));
 app.use(express.static("public"));
 
@@ -39,9 +41,23 @@ app.use(express.static("public"));
 app.use("/api/users", usersRoutes(knex));
 
 // Home page
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+app.get('/polls/:id', (req, res) => {
+  //logic here to find poll with :id
+  if(req.params.id === '1'){
+    res.send('okay!');
+  } else {
+    res.status(404);
+  }
+  // res.status(200).json({poll: {
+  //   name:
+  //   created:
+  //   ...
+  // } })
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
