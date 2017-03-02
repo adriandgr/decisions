1190591d8a43e3862319cb34cf4ea68be28beed3
richard135 <richardhsieh1@hotmail.com>
2017-03-02 23:54:55 +0000

    Changed the querying method, need to send objects still

--- routes/admins.js
+++ routes/admins.js
@@ -13,38 +13,59 @@ module.exports = (knex) => {
     //Still have to get status of poll (true/false)
     //Returns Whole Polls Table
     function pollsTable() {
-      knex.select('*').from('polls')
-      .where('admin_uuid', '=', id)
+      knex('polls')
+      .join('choices','polls.id','choices.poll_id')
+      .join('votes', 'votes.choice_id', 'choices.id')
+      .where('polls.admin_uuid','asd3fed')
+      .select('choices.id','choices.name','rank')
       .then(function(rows) {
-        response['poll'] = rows[0];
-        let poll_id = rows[0].id
-        choicesTable(poll_id)
-      });
-    }
-
-    //Total choices relating to the poll
-    function choicesTable (pollingid){
-      knex.select('*').from('choices').where('poll_id', '=', pollingid)
-      .then(function(rows) {
-        response['choices'] = rows;
-        let totalChoices = rows;
-        votesTableForRanks(totalChoices);
-      });
-    }
-
-    //Getting the ranking of the poll
-    function votesTableForRanks (totalChoices){
-      for (let choice of totalChoices) {
-        // knex.select('*').from('votes').where('choice_id', '=', choice.id)
-        // .then(function(rows) {
-        //   response['ranks'] = rows;
-        // });
-        console.log(choice);
-      }
-      // console.log(response);
+        console.log(rows)
+        res.json(rows);
+      })
     }
 
     pollsTable();
+// FROM polls
+// JOIN choices ON polls.id = choices.poll_id
+// JOIN votes ON votes.choice_id = choices.id
+// WHERE polls.admin_uuid = 'asd3fed';
+
+
+
+
+    // function pollsTable() {
+    //   knex.select('*').from('polls')
+    //   .where('admin_uuid', '=', id)
+    //   .then(function(rows) {
+    //     response['poll'] = rows[0];
+    //     let poll_id = rows[0].id;
+    //     return choicesTable(poll_id);
+    //   })
+    // }
+
+    //Total choices relating to the poll
+    // function choicesTable (pollingid){
+    //   knex.select('*').from('choices').where('poll_id', '=', pollingid)
+    //   .then(function(rows) {
+    //     response['choices'] = rows;
+    //     let totalChoices = rows;
+    //     return votesTableForRanks(totalChoices);
+    //     // console.log(response)
+    //   });
+    // }
+
+    // //Getting the ranking of the poll
+    // function votesTableForRanks (totalChoices){
+    //   for (let choice of totalChoices) {
+    //     knex.select('*').from('votes').where('choice_id', '=', choice.id)
+    //     .then(function(rows) {
+    //       response['ranks'] = rows;
+    //       console.log('Object with all information',response)
+    //     });
+    //   }
+    // }
+
+    // pollsTable();
 
 
 // Show ranks
