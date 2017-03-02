c5e49d2e450e3ce4e8eb3054f55585a63f13c71a
GEDDE5 <geddes.3754@gmail.com>
2017-03-01 19:45:37 -0800

    Adds POST route for /:id and further clarifies meanings for routes

--- routes/polls.js
+++ routes/polls.js
@@ -9,15 +9,23 @@ module.exports = db => {
     res.send(req.params.id);
   });
 
-  route.get('/:id', (req, res) => {
-    let meaning = 'This page is responsible for a given voter\'s view of a poll';
-    res.send(meaning);
-  });
-
   route.post('/', (req, res) => {
-    let meaning = 'This page is responsible for receiving the POST data from the "Create a New Poll" form';
+    let meaning = 'This route is responsible for receiving the POST data from the "Create a New Poll" form';
     meaning += ' and then querying the database to eventually return a response containing relevant data';
     res.send(meaning);
   });
+
+  route.get('/:id', (req, res) => {
+    let meaning = 'This route is responsible for a given voter\'s view of a poll';
+    res.send(meaning);
+  });
+
+  route.post('/:id', (req, res) => {
+    let meaning = 'This route is reponsible for receiving vote data, inserting this data into the database';
+    meaning += ' meaningfully, and then returning updated vote counts';
+    res.send(meaning);
+  });
+
+
   return route;
 };
