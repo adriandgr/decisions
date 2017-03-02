14b35088edfd73d46909cc5ecd8fbd1d0d7d7273
GEDDE5 <geddes.3754@gmail.com>
2017-03-02 00:44:05 -0800

    Adds createVoters function and experiments w/ chaining inserts via passing insert functions along via the created row's id

--- routes/polls.js
+++ routes/polls.js
@@ -12,6 +12,48 @@ module.exports = knex => {
   route.post('/', (req, res) => {
     let meaning = 'This route is responsible for receiving the POST data from the "Create a New Poll" form';
     meaning += ' and then querying the database to eventually return a response containing relevant data';
+
+    function createVoters(poll_id) {
+      [
+        { name: 'Donald', email: 'geddes.3574', voter_uuid: 'asdf' },
+        { name: 'Richard', email: 'an@email.com', voter_uuid: 'fdsa' },
+        { name: req.body.created_by, email: req.creator_email, voter_uuid: 'afsd' }
+      ].forEach(c => {
+        let query = [{
+          name: c.name,
+          email: c.email,
+          poll_id: poll_id,
+          voter_uuid: c.voter_uuid
+        }];
+        knex('voters')
+          .insert(query)
+          .return('id')
+          .then(id => {
+            console.log('    Created voter => ', id);
+          }).catch(err => {
+            console.errer('Error:', err);
+          });
+      });
+    }
+
+    function createChoices(poll_id) {
+      ['a', 'b', 'c'].forEach((c, idx) => {
+        let query = [{
+          name: c,
+          poll_id: poll_id
+        }];
+        knex('choices')
+          .insert(query)
+          .return('id')
+          .then(id => {
+            console.log('  Created choice => id:', id, '\n  => name: c');
+            createVoters(poll_id)
+          }).catch(err => {
+            console.error('Error:', err);
+          } );
+      });
+    }
+
     function createPoll() {
       let query = [{
         name: req.body.name,
@@ -19,31 +61,20 @@ module.exports = knex => {
         creator_email: req.body.creator_email,
         admin_uuid: 'asdfasdfasdf'
       }];
-      console.log(req.body);
+
       knex('polls')
         .insert(query)
-        .then(results => {
-          console.log('Success');
-          res.json(results);
+        .returning('id')
+        .then(id => {
+          console.log('Created poll => id:', id[0]);
+          createChoices(id[0]);
+          return id;
         })
-        .catch(err => { conosle.log('Error: ', err); });
+        .catch(err => {
+          console.log('Error: ', err);
+        });
     }
-    // createPoll();
-    function createChoices() {
-      let query = [{
-        name: 'asdf',
-        poll_id: 1
-      }];
-      knex('choices')
-        .insert(query)
-        .then(results => {
-          if(results.rowCount === 1) {
-            console.log('Success');
-            res.json(results);
-          }
-        }).catch(err => { console.log(err); } );
-    }
-    createChoices();
+    createPoll();
   });
 
   route.get('/:uuid', (req, res) => {
