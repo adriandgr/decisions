f7fa615b734ae15b959396e9a6c8d9d074943bb4
GEDDE5 <geddes.3754@gmail.com>
2017-03-02 21:20:57 -0800

    Creates two functions for /polls/:uuid - 1) getChoicesAndRanks which returns each choice and associated rank - and 2) pullEssentials which queries the database to provide 1) w/ the poll_id

--- routes/polls.js
+++ routes/polls.js
@@ -13,7 +13,7 @@ module.exports = knex => {
     let meaning = 'This route is responsible for receiving the POST data from the "Create a New Poll" form';
     meaning += ' and then querying the database to eventually return a response containing relevant data';
 
-    // Still needs to be tested
+    response = {};
     function createVoters(column) {
       [
         { name: 'Donald', email: 'geddes.3574', voter_uuid: 'asdf' },
@@ -39,7 +39,7 @@ module.exports = knex => {
     };
 
     function createChoices(poll_id) {
-      ['a', 'b', 'c'].forEach(c => {
+      ['basketball', 'hockey', 'c'].forEach(c => {
         let query = [{
           name: c,
           poll_id: poll_id
@@ -84,7 +84,47 @@ module.exports = knex => {
   route.get('/:uuid', (req, res) => {
     let meaning = 'This route is responsible for a given voter\'s view of a poll';
 
-    res.send(meaning);
+    let response = {};
+    // select choices.id, choices.name, votes.rank from choices inner join votes on votes.choice_id = choices.id where choices.poll_id = 1;
+    function getChoicesAndRanks(poll_id) {
+      knex('choices')
+        .select('choices.id', 'choices.name')
+        .join('votes', 'votes.choice_id', 'choices.id')
+        .where('choices.poll_id', poll_id)
+        .sum('votes.rank as borda_rank')
+        .groupBy('name', 'choices.id')
+        .then(results => {
+          // console.log(results);
+          response['choices'] = results;
+          console.log(response);
+          res.json(response);
+        })
+        .catch(err => {
+          console.error(err);
+        })
+    }
+
+    function pollEssentials() {
+      knex('voters')
+        .select('polls.id', 'polls.name', 'created_at')
+        .join('polls', 'polls.id', 'voters.poll_id')
+        .where('voters.voter_uuid', 'sf4ffvc')
+        .then(results => {
+          response['poll'] = results;
+          return results[0].id;
+        })
+        .then(poll_id => {
+          console.log(poll_id);
+          getChoicesAndRanks(poll_id);
+
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+    pollEssentials();
+
+    // res.send(meaning);
   });
 
   route.post('/:uuid', (req, res) => {
