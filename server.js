1d6393dc14244e653afac7dd7711b6e853ca4490
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-03 18:01:12 +0000

    sets up winston and env conditional

--- server.js
+++ server.js
@@ -3,30 +3,34 @@
 require('dotenv').config();
 
 const PORT        = process.env.PORT || 5000;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
+const ENV         = process.env.ENV || 'development';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const sass        = require('node-sass-middleware');
 const path        = require('path');
 const app         = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
+const knexConfig  = require('./knexfile');
+const knex        = require('knex')(knexConfig[ENV]);
+const winston     = require('winston');
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+const usersRoutes = require('./routes/users');
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
 
-// Log knex SQL queries to STDOUT as well
-app.use(knexLogger(knex));
+if (ENV === 'development') {
+  // Load the logger first so all (static) HTTP requests are logged to STDOUT
+  // 'dev' = Concise output colored by response status for development use.
+  //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+  app.use(morgan('dev'));
+  // Log knex SQL queries to STDOUT as well
+  app.use(knexLogger(knex));
+}
 
-app.set("view engine", "ejs");
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(sass({
   src: path.join(__dirname, '/sass'),
@@ -35,14 +39,14 @@ app.use(sass({
   outputStyle: 'expanded',
   prefix: '/styles'
 }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+app.use('/api/users', usersRoutes(knex));
 
 // Home page
-// app.get("/", (req, res) => {
-//   res.render("index");
+// app.get('/', (req, res) => {
+//   res.render('index');
 // });
 
 app.get('/polls/:id', (req, res) => {
@@ -60,5 +64,5 @@ app.get('/polls/:id', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  winston.info('Example app listening on port ' + PORT);
 });
