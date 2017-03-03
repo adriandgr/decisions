e036edf474070ebd866cad5136283ae0dcd20756
GEDDE5 <geddes.3754@gmail.com>
2017-03-03 12:06:24 -0800

    Creates insert object which handles abstraction from routes for inserting into database (only route affected so far is /polls POST)

--- db/lib/helpers.js
+++ db/lib/helpers.js
@@ -3,43 +3,62 @@ module.exports = knex => {
 
     insert: {
 
-      // voters:
-      //   (poll, req) => {
-      //     [
-      //     { name: 'Donald', email: 'geddes.3574', voter_uuid: 'asdf' },
-      //     { name: 'Richard', email: 'an@email.com', voter_uuid: 'fdsa' },
-      //     { name: 'Adrian', email: 'another@email.com', voter_uuid: 'afsd' }
-      //     // { name: req.body.created_by, email: req.creator_email,  voter_uuid: column.admin_uuid }
-      //     ].forEach(v => {
-      //       let query = [{
-      //         name: v.name,
-      //         email: v.email,
-      //         poll_id: poll.poll_id,
-      //         voter_uuid: v.voter_uuid
-      //       }];
-      //       knex('voters')
-      //         .insert(query)
-      //         .returning('id')
-      //     });
-      //   },
+      voters:
+        (poll, req) => {
+          let voters = [
+            { name: 'Donald', email: 'geddes.3574', voter_uuid: 'asdf' },
+            { name: 'Richard', email: 'an@email.com', voter_uuid: 'fdsa' },
+            { name: 'Adrian', email: 'another@email.com', voter_uuid: 'afsd' }
+            // { name: req.body.created_by, email: req.creator_email,  voter_uuid: column.admin_uuid }
+          ];
+          let promises = voters
+            .map(v => {
+              return new Promise((resolve, reject) => {
+                let query = [{
+                  name: v.name,
+                  email: v.email,
+                  poll_id: poll.poll_id,
+                  voter_uuid: v.voter_uuid
+                }];
+                knex('voters')
+                  .insert(query)
+                  .returning('id')
+                  .then(id => {
+                    console.log('    Created voter => ', v.name, '=> id: ', id);
+                    resolve(poll);
+                  })
+                  .catch(err => {
+                    reject(err);
+                  });
+              });
+            });
+          return promises;
+        },
 
       choices:
         poll => {
-          let promises =
-            ['basketball', 'hockey', 'c']
-              .map(c => {
-                return new Promise ((resolve, reject) => {
-                  let query = [{
-                    name: c,
-                    poll_id: poll.id
-                  }];
-                  knex('choices')
-                    .insert(query)
-                    .returning('id')
-                    .then(id => { console.log('Created choice: ', id); });
-                });
+          let promises = [
+            'basketball', 'hockey', 'c'
+          ]
+            .map(c => {
+              return new Promise((resolve, reject) => {
+                let query = [{
+                  name: c,
+                  poll_id: poll.id
+                }];
+                knex('choices')
+                  .insert(query)
+                  .returning('id')
+                  .then(id => {
+                    console.log('  Created choice => id:', id, '\n  => name:', c);
+                    resolve(poll);
+                  })
+                  .catch(err => {
+                    reject(err);
+                  });
               });
-          return Promise.all(promises);
+            });
+          return promises;
         },
 
       pollRow:
@@ -54,7 +73,11 @@ module.exports = knex => {
 
           return knex('polls')
                   .insert(query)
-                  .returning(['id', 'admin_uuid', ]);
+                  .returning(['id', 'admin_uuid'])
+                  .then(poll => {
+                    console.log('Created poll => id:', poll[0].id);
+                    return poll;
+                  });
         }
 
     } // closes create{}
