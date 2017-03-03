6a1665e824b9f911910a3c4e2128aa5241659d4b
richard135 <richardhsieh1@hotmail.com>
2017-03-03 03:27:03 +0000

    Added catch error & refactored code. Still need to add helper function

--- routes/admins.js
+++ routes/admins.js
@@ -17,8 +17,8 @@ module.exports = (knex) => {
       .where('polls.admin_uuid', id)
       .select('choices.id','choices.name','rank')
       .then(function(rows) {
-        response['rankTable'] = rows
-      })
+        response['rankTable'] = rows;
+      }).catch(err =>{throw err});
     }
 
     gettingChoicesNameRankTable();
@@ -30,30 +30,34 @@ module.exports = (knex) => {
       .then(function(rows) {
         response['polls'] = rows;
         res.json(response);
-      })
+      }).catch(err =>{throw err});
     }
+
     gettingPollsTable();
-  });
+
+    });
 
   route.post('/:admin_uuid', (req, res) =>{
     let id = req.params.admin_uuid
-    function checkActive (){
-      knex('polls').where('polls.admin_uuid', '=', id).update('active',true)
-      .then(function(rows){
-          console.log(rows)
-      });
+
+    //Needs to include Mailgun chain here upon submission, informing users polls have ended
+    function checkActive () {
+      knex('polls').where('polls.admin_uuid', '=', id).update('active',false)
     }
     checkActive();
 
-    function upDateTitle(){
+
+
+    function updateTitle() {
+      //Change the name of the question to req.body
       knex('polls').where('polls.admin_uuid', '=', id).update('name',"Where do you want to eat")
-      .then(function(rows){
-          console.log(rows)
-      res.json({success: true});
+      .then(function(rows) {
+        res.json({success: true});
       });
     }
 
-    upDateTitle()
+
+    updateTitle()
   })
 
 
