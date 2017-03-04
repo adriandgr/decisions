c367ab0ace87ad212505ff224efc3092f9ed6587
GEDDE5 <geddes.3754@gmail.com>
2017-03-03 18:28:33 -0800

    Updates dummy data for insert.voters() (adds poll_id)

--- db/lib/helpers.js
+++ db/lib/helpers.js
@@ -6,9 +6,9 @@ module.exports = knex => {
       voters:
         (poll, req) => {
           let voters = [
-            { name: 'Donald', email: 'geddes.3574', voter_uuid: 'asdf' },
-            { name: 'Richard', email: 'an@email.com', voter_uuid: 'fdsa' },
-            { name: 'Adrian', email: 'another@email.com', voter_uuid: 'afsd' }
+            { name: 'Donald', email: 'geddes.3574', voter_uuid: 'asdf', poll_id: 1},
+            { name: 'Richard', email: 'an@email.com', voter_uuid: 'fdsa', poll_id: 2 },
+            { name: 'Adrian', email: 'another@email.com', voter_uuid: 'afsd', poll_id: 2 }
             // { name: req.body.created_by, email: req.creator_email,  voter_uuid: column.admin_uuid }
           ];
           let promises = voters;
